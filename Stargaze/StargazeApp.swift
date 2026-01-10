4b3fe708ce9abcd82ca6e0b630b0117b38ebde66
Hazeline <roshin006@gmail.com>
2026-01-10 13:53:28 +0530

    Initial Commit

---
+++ Stargaze/StargazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StargazeApp.swift
+//  Stargaze
+//
+//  Created by Hazeline. on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct StargazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StargazeTests/StargazeTests.swift
@@ -0,0 +1,17 @@
+//
+//  StargazeTests.swift
+//  StargazeTests
+//
+//  Created by Hazeline. on 1/10/26.
+//
+
+import Testing
+@testable import Stargaze
+
+struct StargazeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
