b2da5d700455f2bf3359bca2b248d4e2839c9089
Hazeline. <roshin006@gmail.com>
2026-02-02 09:55:08 +0530

    Super view for Habits

---
+++ Stargaze/UI/HabitTabView.swift
@@ -0,0 +1,36 @@
+//
+//  HabitTabView.swift
+//  Stargaze
+//
+//  Created by Hazel Nishad on 17/1/26.
+//  Copyright © 2026 Hazel's Garden. Check license for details.
+// 💖✨
+//
+
+import SwiftUI
+import SwiftData
+
+struct HabitTabView: View {
+  
+  @Query(sort: \Habit.sortOrder) var habits: [Habit]
+  @Environment(\.modelContext) var modelContext
+  // MARK: Apparently TabView in page mode has perf issues
+  // MARK: that is somewhat allieviated by a pointless sel binding
+  // MARK: We may need to go UIKit on this.
+  @State var selection: Int
+  
+    var body: some View {
+      return TabView(selection: $selection) {
+        ForEach(habits) {habit in
+          HabitView(habit: habit)
+        }
+      }
+      .ignoresSafeArea()
+      .tabViewStyle(.page(indexDisplayMode: .never))
+    }
+}
+
+#Preview {
+  HabitTabView(selection: 0)
+    .modelContainer(SampleData.shared.modelContainer)
+}
