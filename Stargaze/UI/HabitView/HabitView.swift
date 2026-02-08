80aef9e45eec631fb7f330ce233b31b13d5be389
Hazeline. <roshin006@gmail.com>
2026-02-08 16:00:39 +0530

    Fixed Canvas positioning to have an extended space

--- Stargaze/UI/HabitView/HabitView.swift
+++ Stargaze/UI/HabitView/HabitView.swift
@@ -13,36 +13,63 @@ struct HabitView: View {
 
   let habit: Habit
   // For debug purposes only. Please turn this off when shipping!
-  let showBorder = false
-  let canvasBottomPadding: CGFloat = 16
+  let showBorder: Bool = false
+
+  let canvasVerticalPadding: CGFloat = 16
+  let canvasHorizontalPadding: CGFloat = 24
+  let canvasHeight: CGFloat = 420
+
+  let titleVerticaladding: CGFloat = 40
+  let chromeHorizontalPadding: CGFloat = 24
 
   var body: some View {
     ZStack {
       GradientBackgroundView(showBorder: showBorder, color: habit.color)
         .ignoresSafeArea()
 
-      // Main Content
       VStack(spacing: 0) {
-        TitleView(showBorder: showBorder, title: habit.title, desc: habit.desc)
-
-        GridEmptySpace(
+        TitleView(
           showBorder: showBorder,
-          canvasPadding: canvasBottomPadding
+          title: habit.title,
+          desc: habit.desc
         )
+        .padding(.horizontal, chromeHorizontalPadding)
+
+        ZStack {
+          // For some reason, gridTracker is smaller w/o the emptySpace
+          GridEmptySpace(
+            showBorder: showBorder,
+            canvasHeight: canvasHeight,
+            horizontalPadding: canvasHorizontalPadding,
+            verticalPadding: canvasVerticalPadding,
+          )
+          GridTrackerView(
+            viewModel: GridTrackerViewModel(
+              color: habit.color,
+              checkedDays: habit.checkedDays,
+              horizontalPadding: canvasHorizontalPadding,
+              verticalPadding: canvasVerticalPadding,
+              canvasHeight: canvasHeight,
+            ),
+            showBorder: showBorder,
+          )
+        }
+        .padding(.bottom, canvasVerticalPadding / 2)
+
         GridTrackerFooterView(showBorder: showBorder)
+          .padding(.horizontal, chromeHorizontalPadding)
 
         FooterView(color: habit.color, showBorder: showBorder)
+          .padding(.horizontal, chromeHorizontalPadding)
       }
       .frame(maxWidth: .infinity, maxHeight: .infinity)
-      .padding(.horizontal, 24)
-
-      GridTrackerView(showBorder: showBorder)
-        .padding(.bottom, 2 * canvasBottomPadding)
     }
 
   }
 }
 
 #Preview {
-  HabitView(habit: Habit())
+  HabitView(
+    habit: Habit(color: ["hue": 0.8722, "sat": 0.81, "bri": 0.65, "opa": 1])
+  )
 }
