5798ca0de9974e4479c998e9c8fd325bf8cf41a7
Hazeline. <roshin006@gmail.com>
2026-02-08 16:01:00 +0530

    Created a full grid of stars with state and viewModel

--- Stargaze/UI/HabitView/GridTrackerView/GridTrackerView.swift
+++ Stargaze/UI/HabitView/GridTrackerView/GridTrackerView.swift
@@ -10,61 +10,51 @@
 import SwiftUI
 
 struct GridTrackerView: View {
+  @State private var isShowingPopover = false
+  @State var viewModel: GridTrackerViewModel
 
   let showBorder: Bool
-
-  let nRows = 21
-  let nCols = 18
-  let starSize = 8.0
-
-  let canvasHeight: CGFloat = 420
-  let horizontalPadding: CGFloat = 24
-
-  @State private var selectedStarGrid: CGRect?
-  let dayIsComplete: Set = [
-    Point(r: 2, c: 0), Point(r: 2, c: 1), Point(r: 0, c: 1), Point(r: 0, c: 0),
-    Point(r: 0, c: 4), Point(r: 0, c: 7), Point(r: 1, c: 5),
-  ]
 
   var body: some View {
     ZStack {
       GeometryReader { geom in
         TimelineView(.animation) { timeline in
-          Canvas { context, size in
+          Canvas {
+            context,
+            size in
 
             // TODO: Make random stars "shine" periodically
             //          let now = timeline.date.timeIntervalSinceReferenceDate
             //          let angle = now.remainder(dividingBy: 3) * 120
 
-            let canvasOrigin = CGPoint(
-              x: horizontalPadding,
-              y: (size.height / 2) - (canvasHeight / 2)
-            )
-
-            for i in 0..<nRows {
-              for j in 0..<nCols {
-
-                if i * nCols + j >= 365 {
-                  return
-                }
-
-                drawStarGrid(
-                  context: context,
-                  size: size,
-                  canvasOrigin: canvasOrigin,
-                  angle: Double.random(in: 0...45),
-                  row: i,
-                  col: j
-                )
-
-              }
+            for pointsData in viewModel.starPointsData {
+              drawStarGrid(
+                context: context,
+                size: size,
+                starPoint: pointsData,
+              )
             }
+
+          }
+          .onAppear {
+            guard !viewModel.hasDataBeenSet else { return }
+            let size = geom.size
+            initializeCanvas()
+            initalizeStars(size: size)
+          }
+          .onChange(of: geom.size) {
+            let size = geom.size
+            initalizeStars(size: size)
           }
           .border(showBorder ? Color(.tertiaryLabel) : .clear)
           .gesture(
             DragGesture(minimumDistance: 0, coordinateSpace: .local)
               .onChanged { value in
-
+                handleDragGesture(value: value)
+              }
+              .onEnded { value in
+                viewModel.tappedStarPoint = nil
+                isShowingPopover = true
               }
           )
         }
@@ -75,51 +65,137 @@ struct GridTrackerView: View {
         .border(showBorder ? Color(.tertiaryLabel) : .clear)
         .frame(
           maxWidth: .infinity,
-          minHeight: canvasHeight,
-          maxHeight: canvasHeight,
+          minHeight: viewModel.canvasHeight,
+          maxHeight: viewModel.canvasHeight,
         )
-        .padding(.horizontal, horizontalPadding)
+        .padding(.horizontal, viewModel.horizontalPadding)
+    }
+    .frame(
+      maxWidth: .infinity,
+      maxHeight: viewModel.canvasHeight + (2 * viewModel.verticalPadding)
+    )
+  }
+
+  // *************** Struct Functions Below ******************
+
+  func initializeCanvas() {
+    viewModel.hasDataBeenSet = true
+    viewModel.getCheckedDaysSet()
+  }
+
+  func initalizeStars(size: CGSize) {
+    viewModel.initializeStars(size: size)
+  }
+
+  func handleDragGesture(
+    value: DragGesture.Value,
+  ) {
+    let closestStarPoint = viewModel.handleDragGesture(value: value)
+
+    if closestStarPoint.point != viewModel.tappedStarPoint {
+
+      // You have to be close to a star to interact
+      // Squared because the euclidean dist isn't sqrt'd
+      let interactRadius = 16.0
+      guard closestStarPoint.dist < pow(interactRadius, 2) else {
+        return
+      }
+
+      viewModel.tappedStarPoint = closestStarPoint.point
+      Haptics.shared.play(.rigid)
     }
   }
+
+  func moveCanvasToStar(
+    context: inout GraphicsContext,
+    for starPoint: (
+      cgPoint: CGPoint, intPoint: Point, angle: Double, isChecked: Bool
+    ),
+  ) {
+    // Moving them into place (-half starSize because we want topleft)
+    context.translateBy(
+      x: starPoint.cgPoint.x - (viewModel.starSize / 2),
+      y: starPoint.cgPoint.y - (viewModel.starSize / 2)
+    )
+  }
+
+  func rotateStarByAngle(
+    context: inout GraphicsContext,
+    for starPoint: (
+      cgPoint: CGPoint, intPoint: Point, angle: Double, isChecked: Bool
+    ),
+  ) {
+    // Moving the pivot to the center before rotating them
+    context.translateBy(
+      x: (viewModel.starSize / 2),
+      y: (viewModel.starSize / 2)
+    )
+    context.rotate(by: .degrees(starPoint.angle))
+    context.translateBy(
+      x: -(viewModel.starSize / 2),
+      y: -(viewModel.starSize / 2)
+    )
+  }
+
+  // ************ The DRAW function is here! ****************
 
   func drawStarGrid(
     context: GraphicsContext,
     size: CGSize,
-    canvasOrigin: CGPoint,
-    angle: Double,
-    row: Int,
-    col: Int
+    starPoint: (
+      cgPoint: CGPoint, intPoint: Point, angle: Double, isChecked: Bool
+    ),
   ) {
 
     var star = context.resolve(Image("Star"))
-    star.shading = .color(Color(.tertiaryLabel))
+    star.shading =
+      starPoint.isChecked
+      ? .color(
+        ColorMananger.toColorPrimary(color: viewModel.color)
+      )
+      : .color(
+        Color(.quaternaryLabel).mix(
+          with: ColorMananger.toColorPrimary(color: viewModel.color),
+          by: 0.1
+        )
+      )
 
-    let frame = CGRect(
-      origin: CGPoint(x: 0, y: 0),
-      size: CGSize(width: starSize, height: starSize)
-    )
-    let canvasWidth = size.width - (2 * horizontalPadding)
+    if let tappedStarPoint = viewModel.tappedStarPoint {
+      if Point(r: starPoint.intPoint.r, c: starPoint.intPoint.c)
+        == tappedStarPoint
+      {
+        star.shading = .color(
+          ColorMananger.toColorSecondary(color: viewModel.color)
+        )
+      }
+    }
+
     var starContext = context
-
-    // Moving them into place
-    starContext.translateBy(
-      x: canvasOrigin.x
-        + (Double(col) * ((canvasWidth - starSize) / Double(nCols - 1))),
-      y: canvasOrigin.y
-        + (Double(row) * ((canvasHeight - starSize) / Double(nRows - 1)))
-    )
-
-    // Moving the pivot to the center before rotating them
-    starContext.translateBy(x: (starSize / 2), y: (starSize / 2))
-    starContext.rotate(by: .degrees(angle))
-    starContext.translateBy(x: -(starSize / 2), y: -(starSize / 2))
-
-    starContext.draw(star, in: frame)
+    moveCanvasToStar(context: &starContext, for: starPoint)
+    rotateStarByAngle(context: &starContext, for: starPoint)
+    starContext.draw(star, in: viewModel.frame)
 
     //    starContext.stroke(Rectangle().path(in: frame), with: .foreground)
   }
 }
 
 #Preview {
-  GridTrackerView(showBorder: true)
+  GridTrackerView(
+    viewModel: GridTrackerViewModel(
+      color: ["hue": 1, "sat": 0, "bri": 1, "opa": 1],
+      checkedDays: [
+        CheckedDays(date: Date(timeIntervalSince1970: 1_736_208_000)),
+        CheckedDays(date: Date(timeIntervalSince1970: 1_736_640_000)),
+        CheckedDays(date: Date(timeIntervalSince1970: 1_736_899_200)),
+        CheckedDays(date: Date(timeIntervalSince1970: 1_737_244_800)),
+        CheckedDays(date: Date(timeIntervalSince1970: 1_737_417_600)),
+        CheckedDays(date: Date(timeIntervalSince1970: 1_737_676_800)),
+        CheckedDays(date: Date(timeIntervalSince1970: 1_737_936_000)),
+      ],
+      horizontalPadding: 24,
+      verticalPadding: 16,
+      canvasHeight: 420,
+    ),
+    showBorder: true
+  )
 }
