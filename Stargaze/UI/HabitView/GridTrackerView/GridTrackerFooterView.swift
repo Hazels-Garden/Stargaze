5a9388bd2da921dd51056dc20a377d62d09192a1
Hazeline. <roshin006@gmail.com>
2026-02-10 21:05:46 +0530

    added full chevron func in grid footer

--- Stargaze/UI/HabitView/GridTrackerView/GridTrackerFooterView.swift
+++ Stargaze/UI/HabitView/GridTrackerView/GridTrackerFooterView.swift
@@ -15,14 +15,29 @@ struct GridTrackerFooterView: View {
   @Environment(AppState.self) private var appState
   @State var daysRemaining: Int = 365
 
+  var disablePreviousChevron: Bool {
+    appState.selectedYear <= 2010
+  }
+
+  var disableNextChevron: Bool {
+    appState.selectedYear > appState.currentYear + 5
+  }
+
   var body: some View {
     HStack {
       HStack {
         Button("Previous Year", systemImage: "chevron.left") {
-          appState.selectedYear -= 1
+          onPreviousChevron()
         }
-        .buttonStyle(ChevronButton(buttonType: .small, buttonDir: .leading))
+        .buttonStyle(
+          ChevronButton(
+            buttonType: .small,
+            buttonDir: .leading,
+            isDisabled: disablePreviousChevron
+          )
+        )
         .border(showBorder ? .green : .clear)
+        .disabled(disablePreviousChevron)
 
         Text("\(appState.selectedYear.description)")
           .SGMono()
@@ -30,10 +45,17 @@ struct GridTrackerFooterView: View {
           .animation(.smooth(duration: 0.25), value: appState.selectedYear)
 
         Button("Next Year", systemImage: "chevron.right") {
-          appState.selectedYear += 1
+          onNextChevron()
         }
-        .buttonStyle(ChevronButton(buttonType: .small, buttonDir: .trailing))
+        .buttonStyle(
+          ChevronButton(
+            buttonType: .small,
+            buttonDir: .trailing,
+            isDisabled: disableNextChevron
+          )
+        )
         .border(showBorder ? .green : .clear)
+        .disabled(disableNextChevron)
       }
       Spacer()
       Text("\(daysRemaining) days left")
@@ -52,12 +74,44 @@ struct GridTrackerFooterView: View {
     }
   }
 
-  func getDaysRemaining() {
-    appState.calculateDaysInSelectedYear()
-    if appState.selectedYear == appState.currentYear {
-      daysRemaining = appState.daysInSelectedYear - appState.currentDayOfYear
+  func onPreviousChevron() {
+    appState.selectedYear -= 1
+    if appState.selectedYear != appState.currentYear {
+      appState.selectedDate = Calendar.current
+        .date(
+          from: DateComponents(
+            year: appState.selectedYear,
+            month: 1,
+            day: 1
+          )
+        )!
     } else {
-      daysRemaining = appState.daysInSelectedYear
+      appState.selectedDate = appState.currentDate
+    }
+  }
+
+  func onNextChevron() {
+    appState.selectedYear += 1
+    if appState.selectedYear != appState.currentYear {
+      appState.selectedDate = Calendar.current
+        .date(
+          from: DateComponents(
+            year: appState.selectedYear,
+            month: 1,
+            day: 1
+          )
+        )!
+    } else {
+      appState.selectedDate = appState.currentDate
+    }
+  }
+
+  func getDaysRemaining() {
+    let daysInSelectedYear = appState.calculateDaysInSelectedYear()
+    if appState.selectedYear == appState.currentYear {
+      daysRemaining = daysInSelectedYear - appState.currentDate.dayOfYear
+    } else {
+      daysRemaining = daysInSelectedYear
     }
   }
 
