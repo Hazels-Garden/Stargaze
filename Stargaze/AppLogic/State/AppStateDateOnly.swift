8b91decc586d840f5cd6acbfae7a1896697d7bbe
Hazeline. <roshin006@gmail.com>
2026-02-14 14:10:00 +0530

    DateOnly refactor beginnings

---
+++ Stargaze/AppLogic/State/AppStateDateOnly.swift
@@ -0,0 +1,73 @@
+//
+//  AppState.swift
+//  Stargaze
+//
+//  Created by Hazel Nishad on 8/2/26.
+//  Copyright © 2026 Hazel's Garden. Check license for details.
+// 💖✨
+//
+
+import Foundation
+import SwiftUI
+
+@MainActor
+@Observable
+final class AppStateDateOnly {
+
+  static let shared = AppStateDateOnly(
+    currentYear: Calendar(identifier: .gregorian).component(.year, from: .now),
+    selectedYear: Calendar(identifier: .gregorian).component(.year, from: .now),
+    currentDate: DateOnly.now(),  // This is a custom extension that returns date at noon.
+    selectedDate: DateOnly.now(),
+  )
+
+  let calendar = Calendar(identifier: .gregorian)
+  let currentYear: Int
+  var selectedYear: Int
+  let currentDate: DateOnly
+  var selectedDate: DateOnly
+
+  private init(
+    currentYear: Int,
+    selectedYear: Int,
+    currentDate: DateOnly,
+    selectedDate: DateOnly,
+  ) {
+    self.currentYear = currentYear
+    self.selectedYear = selectedYear
+    self.currentDate = currentDate
+    self.selectedDate = selectedDate
+  }
+
+  func calculateDaysInSelectedYear() -> Int {
+    let dateComponents = DateComponents(year: self.selectedYear)
+    let date = calendar.date(from: dateComponents)!
+    let range = calendar.range(of: .day, in: .year, for: date)!
+    return range.count
+  }
+
+  func formattedDateFromSelectedDate() -> String {
+    let dateFormatter = DateFormatter()
+    let userLocaleDateFormat = DateFormatter.dateFormat(
+      fromTemplate: "MMdd",
+      options: 0,
+      locale: Locale.current
+    )
+    dateFormatter.dateFormat = "EEEE, \(userLocaleDateFormat ?? "01/01")"
+    let dateString = dateFormatter.string(
+      from: Date.from(dateOnly: self.selectedDate)!
+    )
+    return dateString
+  }
+
+  func calculateDateFromSelectedDayOfYear(dayOfYear: Int) -> DateOnly? {
+    var dateComponents = DateComponents()
+    dateComponents.year = self.selectedYear
+    dateComponents.day = dayOfYear
+    if let date = calendar.date(from: dateComponents) {
+      return DateOnly.from(date: date)
+    } else {
+      return nil
+    }
+  }
+}
---
+++ Stargaze/AppLogic/Utility/CustomTypes/DateOnly.swift
@@ -0,0 +1,50 @@
+//
+//  DateOnly.swift
+//  Stargaze
+//
+//  Created by Hazel Nishad on 14/2/26.
+//  Copyright © 2026 Hazel's Garden. Check license for details.
+// 💖✨
+//
+
+import Foundation
+
+struct DateOnly: Codable, Hashable, Comparable {
+  static func < (lhs: DateOnly, rhs: DateOnly) -> Bool {
+    Date.from(year: lhs.year, month: lhs.month, day: lhs.day)! < Date.from(year: rhs.year, month: rhs.month, day: rhs.day)!
+  }
+  
+  static func == (lhs: DateOnly, rhs: DateOnly) -> Bool {
+    Date.from(year: lhs.year, month: lhs.month, day: lhs.day)! == Date.from(year: rhs.year, month: rhs.month, day: rhs.day)!
+  }
+
+  var id = UUID()
+  static var calendar = Calendar(identifier: .gregorian)
+  var day: Int
+  var month: Int
+  var year: Int
+  
+  init(id: UUID = UUID(), day: Int, month: Int, year: Int = DateOnly.calendar.component(.year, from: Date.now)) {
+    self.id = id
+    self.day = day
+    self.month = month
+    self.year = year
+  }
+  
+  static func now() -> DateOnly {
+    let components = Date.get(from: Date.now, with: [.day, .month, .year])
+    return DateOnly(day: components.day!, month: components.month!, year: components.year!)
+  }
+  
+  static func from(date: Date) -> DateOnly {
+    let components = Date.get(from: date, with: [.day, .month, .year])
+    return DateOnly(day: components.day!, month: components.month!, year: components.year!)
+  }
+  
+  func dayOfYear() -> Int {
+    let date = Date.from(
+      dateOnly: DateOnly(day: self.day, month: self.month, year:self.year)
+    )
+    return date?.dayOfYear ?? -1
+  }
+}
